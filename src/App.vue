49a29989203c17191249b74caa6a0d23b5037eea
dranyem <davidmeynard@yahoo.com.ph>
2020-03-17 20:38:22 -0600

    created the button component including its method and style

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Button/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Button from './components/Button.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Button
   }
 }
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p @click="toggleView">Grid/List</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Button",
+        methods: {
+            toggleView(){
+                this.$root.$emit('toggle');
+            },
+        }
+    }
+</script>
+
+<style scoped>
+    p{
+        padding: 5px;
+        display: inline-block;
+        border: 1px solid black;
+    }
+</style>
