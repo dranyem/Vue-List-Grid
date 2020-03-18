3fc3366f15ab02079132881e2cf774d37bd923aa
dranyem <davidmeynard@yahoo.com.ph>
2020-03-17 21:24:09 -0600

    added tweet component, worked on style of the site

---
+++ src/components/Tweet.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="tweet">
+        <h1>{{this.tweetData.author}}</h1>
+        <p>{{ this.tweetData.content }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Tweet",
+        props: {
+            tweetData: Object,
+        }
+        
+    }
+</script>
+
+<style scoped>
+    #tweet{
+        display: inline-block;
+        border: 1px solid black;
+        padding: 5px;
+        background-color: lightblue;
+        box-shadow: 5px 5px gray;
+    }
+</style>
--- src/components/TweetList.vue
+++ src/components/TweetList.vue
@@ -1,19 +1,58 @@
 <template>
-    <div id="tweet-container">
-
+    <div class="tweet-container cards">
+        <Tweet class="" v-for="(tweet,index) in tweets" :key="index" :tweetData="tweet"/>
     </div>
 </template>
 
 <script>
+import Tweet from './Tweet.vue'
     export default {
-        name: "TweetList"
-    }
+        name: "TweetList",
+        components: {
+            Tweet,
+        },
+        data(){
+            return {
+                tweets: [
+                    {
+                        author: "Meynard",
+                        content: "My first Tweet"
+                    },
+                    {
+                        author: "Bob",
+                        content: "Hello tweeter"
+                    },
+                    {
+                        author: "Jane",
+                        content: "Hello World"
+                    },
+                    {
+                        author: "Peter",
+                        content: "Hi everyone!"
+                    },
+                    {
+                        author: "John",
+                        content: "Something interesting"
+                    },
+                ],
+            };
+        },
+    }    
 </script>
 
 <style scoped>
-    #tweet-container{
+    .tweet-container{
+        display: grid;
+        gap: 10px;
         width: 90vw;
         height: 90vh;
         border: 1px solid black;
+    }
+    .cards{
+        grid-template-columns:repeat( auto-fill, minmax(250px, 1fr) );
+        text-overflow: ellipsis;
+    }
+    .list{
+        grid-template-columns: 1fr;
     }
 </style>
